5e8ce47f4a4f752a2d00cd4b135b4b67d8f86189
Ivan Egorov <vany.egorov@gmail.com>
2019-05-24 19:02:50 +0300

    update .emacs.d/init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,7 @@
 (require 'cl-lib)
 
 (defvar my-packages
-  '(ack-and-a-half neotree)
+  '(ack-and-a-half neotree helm)
   "A list of packages to ensure are installed at launch.")
 
 (defun my-packages-installed-p ()
@@ -32,7 +32,7 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 
-(setq org-agenda-files (list "/mnt/d/vm/debian/gl-ce.int/transcoder/ctl/docs/roadmap.org"))
+(setq org-agenda-files (list "/mnt/d/vm/debian/gl-ce.int/transcoder/docs/roadmap.org"))
 (setq org-log-done t)
 
 (setq org-tags-column 120)
@@ -41,3 +41,25 @@
 ; ### neotree
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil)
+ '(package-selected-packages (quote (helm neotree ack-and-a-half))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+; ### helm
+(require 'helm-config)
+(require 'helm)
+(helm-mode 1)
+(global-set-key (kbd "M-l") 'helm-buffers-list)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
